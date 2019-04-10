09c91547a8236b01e32aa59d210ffab3f5f6974d
krittiyaclark <krittiyaclark@gmail.com>
2019-04-10 16:47:02 -0400

    Post the data with REST API

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 
-const Home = () => {
-    return (
-      <div className="container">
-        <h4 className="center">Home</h4>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum, odit deserunt. Autem distinctio quos quae accusamus itaque quo nihil numquam vitae, quia similique aut quam explicabo labore quod optio nostrum.</p>
-      </div>
-    )
+class Home extends Component {
+    // Store the data from posts
+    state = {
+        posts: [ ]
+    }
+    componentDidMount() {
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+            .then(res => {
+                console.log(res)
+                // Take the data and add to the posts array object above
+                // Use Slice to grap the data from 0 to 10
+                this.setState({
+                    posts: res.data.slice(0,10)
+                })
+            })
+    }
+    render(){
+        const { posts } = this.state
+        const postList = posts.length ? (
+          posts.map(post => {
+            return (
+              <div className="post card" key={post.id}>
+                <div className="card-content">
+                  <span className="card-title">{post.title}</span>
+                  <p>{post.body}</p>
+                </div>
+              </div>
+            )
+          })
+        ) : (
+          <div className="center">No posts to show</div>
+        );
+        return (
+            <div className="container">
+              <h4 className="center">Home</h4>
+              {postList}
+            </div>
+          )
+    }
 }
 
 export default Home;
