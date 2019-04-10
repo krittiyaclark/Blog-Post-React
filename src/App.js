7a6884048335220e12edbcf08a17cf082ac3b0af
krittiyaclark <krittiyaclark@gmail.com>
2019-04-10 17:23:11 -0400

    Set up Route Parameters

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/About';
 import Contact from './components/Contact';
-
+import Post from './components/Post';
 
 class App extends Component {
   render() {
@@ -12,9 +12,10 @@ class App extends Component {
       <BrowserRouter>
         <div className="App">
           <Navbar />
-          <Route exact path='/' component={Home}/>
+          <Route exact path='/' component={Home} />
           <Route path='/about' component={About} />
           <Route path='/contact' component={Contact} />
+          <Route path='/:post_id' component={Post} />
         </div>
       </BrowserRouter>
     );
