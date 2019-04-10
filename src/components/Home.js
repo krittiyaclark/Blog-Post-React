ee7cb0b3762d3dba8e2af0886a327f8fcbe449b1
krittiyaclark <krittiyaclark@gmail.com>
2019-04-10 17:59:34 -0400

    Post individual post on the page with Switch

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 class Home extends Component {
     // Store the data from posts
@@ -24,8 +25,10 @@ class Home extends Component {
             return (
               <div className="post card" key={post.id}>
                 <div className="card-content">
-                  <span className="card-title">{post.title}</span>
-                  <p>{post.body}</p>
+                    <Link to={'/' + post.id}>
+                    <span className="card-title">{post.title}</span>
+                    </Link>
+                    <p>{post.body}</p>
                 </div>
               </div>
             )
