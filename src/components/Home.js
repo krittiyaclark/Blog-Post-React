39720b021bd53238662ed52708d256f000c205c4
krittiyaclark <krittiyaclark@gmail.com>
2019-04-16 06:42:04 -0400

    Connect Blog Detail Page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,25 +1,27 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+// import axios from 'axios';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux'
 
 class Home extends Component {
     // Store the data from posts
-    state = {
-        posts: [ ]
-    }
-    componentDidMount() {
-        axios.get('https://jsonplaceholder.typicode.com/posts')
-            .then(res => {
-                console.log(res)
-                // Take the data and add to the posts array object above
-                // Use Slice to grap the data from 0 to 10
-                this.setState({
-                    posts: res.data.slice(0,10)
-                })
-            })
-    }
+    // state = {
+    //     posts: [ ]
+    // }
+    // componentDidMount() {
+    //     axios.get('https://jsonplaceholder.typicode.com/posts')
+    //         .then(res => {
+    //             console.log(res)
+    //             // Take the data and add to the posts array object above
+    //             // Use Slice to grap the data from 0 to 10
+    //             this.setState({
+    //                 posts: res.data.slice(0,10)
+    //             })
+    //         })
+    // }
     render(){
-        const { posts } = this.state
+        // const { posts } = this.state
+        const { posts } = this.props
         const postList = posts.length ? (
           posts.map(post => {
             return (
@@ -45,4 +47,10 @@ class Home extends Component {
     }
 }
 
-export default Home;
+const mapStateToProps = (state) => {
+  return {
+    posts: state.posts
+  }
+}
+
+export default connect(mapStateToProps)(Home);
